9624fe8f2fcd5f84784fd468c36eef83a68641ce
Technoboy10 <cchudson98@gmail.com>
2013-06-20 10:36:54 -0500

    Update PianoBot.py

--- PianoBot.py
+++ PianoBot.py
@@ -4,6 +4,8 @@ import os
 from subprocess import call
 from getpass import getpass
 import urllib
+import imaplib
+import smtplib
 def generate_id(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for x in range(size))
 def imap_get_latest(username, password, imap):
